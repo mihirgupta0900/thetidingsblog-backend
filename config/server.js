bef9f43bfa8a9a389adcb64d1aefbc780a955106
Mihir Gupta <mihirgupta0900@gmail.com>
2020-11-02 18:30:51 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '852ae992378dc87d49cb68be9d35bcba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10283658-6660-4f63-92e6-c569bac929cb'
+};
