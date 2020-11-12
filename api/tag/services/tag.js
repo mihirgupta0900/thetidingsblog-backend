ba035504410a9297d66c4a20e4f578c014865939
Mihir Gupta <mihirgupta0900@gmail.com>
2020-11-12 21:32:30 +0530

    added more layers to tag query

--- api/tag/services/tag.js
+++ api/tag/services/tag.js
@@ -1,8 +1,53 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async find(params, populate) {
+    // get array of tags
+    let tags = await strapi.query("tag").find(params, populate);
+
+    // map through the array to get access to each array of blogs
+    let newTags = await Promise.all(
+      tags.map(async (tag) => {
+        let blogs = tag.blogs;
+
+        // map through blogs
+        let newBlogs = await Promise.all(
+          blogs.map(async (blog) => {
+            // get the author id, which is the author field
+            let authorId = blog.author;
+
+            // get the author id, which is the category field
+            let categoryId = blog.category;
+
+            // get the author of the blog
+            let author = await strapi
+              .query("author")
+              .findOne({ id: authorId }, populate);
+            // get the category of the blog
+            let category = await strapi
+              .query("category")
+              .findOne({ id: categoryId }, populate);
+
+            let updatedBlog = {
+              ...blog,
+              author,
+              category,
+            };
+            return updatedBlog;
+          })
+        );
+        let updatedTag = {
+          ...tag,
+          blogs: newBlogs,
+        };
+        return updatedTag;
+      })
+    );
+    return newTags;
+  },
+};
