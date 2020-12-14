f9601a126aab25d4bd16acaa49514912123c4071
Mihir Gupta <mihirgupta0900@gmail.com>
2020-12-15 01:25:04 +0530

    final push before heroku host

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,6 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '10283658-6660-4f63-92e6-c569bac929cb'
+  jwtSecret: process.env.JWT_SECRET || "10283658-6660-4f63-92e6-c569bac929cb",
+  jwt: {
+    expiresIn: "100y",
+  },
 };
